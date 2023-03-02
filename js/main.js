ca442e5f451f7d35b268f33a437e562c4cb03f55
Mohammad Sadra Kiantash <sadracoderbusiness@gmail.com>
2023-03-02 16:22:30 +0330

    I have modified some of the existing code and I added sections for website products and some Images for products and I added some icons

--- js/main.js
+++ js/main.js
@@ -81,3 +81,19 @@ let carouselTimer = setInterval(function () {
 
   if (slideIndex != -1) moveToSlide(currentSlide, nextSlide, targetDot);
 }, 5000);
+
+//Writing a function the gets the browser width
+
+function getWidth() {
+  if (self.innerWidth) {
+    return self.innerWidth;
+  }
+
+  if (document.documentElement && document.documentElement.clientWidth) {
+    return document.documentElement.clientWidth;
+  }
+
+  if (document.body) {
+    return document.body.clientWidth;
+  }
+}
